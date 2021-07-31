348e35b5ca515208511d642b919bf313b378e476
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-07-31 16:10:51 +0600

    length

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Age from './components/Age';
 import Book from './components/Book';
+import Length from './components/Length';
 
 function App() {
   return (
     <div className="App">
     <Age year ='2002'/>
     <Book name ='Alchemist' author='Paulo Coelho' />
+    <Length example = "funny summer also right left;"/>
 
     </div>
   );
---
+++ src/components/Length.js
@@ -0,0 +1,6 @@
+function Length(props){
+   return <div>
+        "{props.example}" is <strong>{props.example.length}</strong>
+    </div>
+}
+export default Length;
